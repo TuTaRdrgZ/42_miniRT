e0a6a834aa55b632d1a557a73a994ed0df7e82e6
Bautista Rodriguez <bautirodri@gmail.com>
2024-05-30 17:57:18 +0200

    hit object

--- src/intersection/hit_any.c
+++ src/intersection/hit_any.c
@@ -1,4 +1,5 @@
 #include "miniRT.h"
+#include <float.h>
 #include "structures.h"
 #include "vector.h"
 
@@ -14,16 +15,13 @@ float	create_hit_point(t_obj **obj, t_ray *ray, t_hit *hit)
 	t_vec	normal;
 	t_vec	hit_point;
 
-	hit->didItHit = 0;
 	if ((*obj)->type == SP)
 	{
 		if (hit_sphere(ray, (*obj)->object, &hit_point, &normal))
 		{
 			set_intersect(hit, &normal, &hit_point);
 			hit->rgb = get_sphere_rgb((*obj)->object);
-			return (distance_vec(ray->origin,
-					subtract_vec(hit_point,
-						ray->origin)));
+			return (distance_vec(ray->origin, hit_point));
 		}
 	}
 	if ((*obj)->type == PL)
@@ -32,9 +30,7 @@ float	create_hit_point(t_obj **obj, t_ray *ray, t_hit *hit)
 		{
 			set_intersect(hit, &normal, &hit_point);
 			hit->rgb = get_plane_rgb((*obj)->object);
-			return (distance_vec(ray->origin,
-					subtract_vec(hit_point,
-						ray->origin)));
+			return (distance_vec(ray->origin, hit_point));
 		}
 	}
 	if ((*obj)->type == CY)
@@ -43,9 +39,7 @@ float	create_hit_point(t_obj **obj, t_ray *ray, t_hit *hit)
 		{
 			set_intersect(hit, &normal, &hit_point);
 			hit->rgb = get_cylinder_rgb((*obj)->object);
-			return (distance_vec(ray->origin,
-					subtract_vec(hit_point,
-						ray->origin)));
+			return (distance_vec(ray->origin, hit_point));
 		}
 	}
 	return (-1);
@@ -55,19 +49,19 @@ bool	simple_check_hit(t_obj *obj, t_hit *hit, t_vec light)
 {
 	t_ray	ray;
 	t_obj	*tmp;
-	float	max_distance;
+	double  max_distance;
 	t_hit	current_hit;
 	float	distance;
 
 	ray.origin = add_vec(hit->hit_point, hit->normal);
 	ray.direction = subtract_vec(light, ray.origin);
-	max_distance = distance_vec(ray.origin, light);
+    max_distance = distance_vec(ray.origin, light);
 	tmp = obj;
 	while (tmp)
 	{
 		current_hit.didItHit = 0;
 		distance = create_hit_point(&tmp, &ray, &current_hit);
-		if (current_hit.didItHit == 1 && distance < max_distance)
+		if (current_hit.didItHit == 1 && (distance <= max_distance))
             return (true);
 		tmp = tmp->next;
 	}
